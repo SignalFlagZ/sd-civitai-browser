75b6292f99c77bf888cf1d9b32b8545e9ff54212
SignalFlagZ <Signal0Flag0Z@gmail.com>
2023-08-24 20:50:03 +0900

    Format code

--- scripts/file_manage.py
+++ scripts/file_manage.py
@@ -268,3 +268,86 @@ def download_file(url, file_name):
 #
 #    # Close the progress bar
 #    progress.close()
+
+def download_file2(folder, filename,  url):
+
+    makedirs(folder)
+    file_name = os.path.join(folder, filename)
+    #thread = threading.Thread(target=download_file, args=(url, filepath))
+
+    # Maximum number of retries
+    max_retries = 5
+
+    # Delay between retries (in seconds)
+    retry_delay = 10
+
+    exitGenerator=False
+    while not exitGenerator:
+        # Check if the file has already been partially downloaded
+        if os.path.exists(file_name):
+            # Get the size of the downloaded file
+            downloaded_size = os.path.getsize(file_name)
+
+            # Set the range of the request to start from the current size of the downloaded file
+            headers = {"Range": f"bytes={downloaded_size}-"}
+        else:
+            downloaded_size = 0
+            headers = {}
+
+        # Split filename from included path
+        tokens = re.split(re.escape('\\'), file_name)
+        file_name_display = tokens[-1]
+
+        # Initialize the progress bar
+        yield "Connecting..."
+        progressConsole = tqdm(total=1000000000, unit="B", unit_scale=True, desc=f"Downloading {file_name_display}", initial=downloaded_size, leave=False)
+        prg = downloaded_size
+        # Open a local file to save the download
+        with open(file_name, "ab") as f:
+            while not exitGenerator:
+                try:
+                    # Send a GET request to the URL and save the response to the local file
+                    response = requests.get(url, headers=headers, stream=True)
+                    # Get the total size of the file
+                    total_size = int(response.headers.get("Content-Length", 0))
+
+                    # Update the total size of the progress bar if the `Content-Length` header is present
+                    if total_size == 0:
+                        total_size = downloaded_size
+                    progressConsole.total = total_size
+                    # Write the response to the local file and update the progress bar
+                    for chunk in response.iter_content(chunk_size=8388608):
+                        if chunk:  # filter out keep-alive new chunks
+                            f.write(chunk)
+                            progressConsole.update(len(chunk))
+                            prg += len(chunk)
+                            yield f'{round(prg/1048576)}MB / {round(total_size/1048576)}MB'
+                    downloaded_size = os.path.getsize(file_name)
+                    # Break out of the loop if the download is successful
+                    break
+                except GeneratorExit:
+                    exitGenerator=True
+                    return
+                except ConnectionError as e:
+                    # Decrement the number of retries
+                    max_retries -= 1
+
+                    # If there are no more retries, raise the exception
+                    if max_retries == 0:
+                        raise e
+
+                    # Wait for the specified delay before retrying
+                    time.sleep(retry_delay)
+        # Close the progress bar
+        exitGenerator=True
+        progressConsole.close()
+        downloaded_size = os.path.getsize(file_name)
+        # Check if the download was successful
+        if downloaded_size >= total_size:
+            print(Fore.LIGHTCYAN_EX + f"Save: {file_name_display}" + Style.RESET_ALL)
+            yield 'Downloaded'
+
+        else:
+            print(f"Error: File download failed. Retrying... {file_name_display}")
+            yield 'Failed'
+        return
--- scripts/gui_civitai.py
+++ scripts/gui_civitai.py
@@ -1,115 +1,13 @@
 import gradio as gr
-from html import escape
 from modules import script_callbacks
 import modules.scripts as scripts
 from scripts.civitai_api import civitaimodels
 from scripts.file_manage import extranetwork_folder, isExistFile,\
-                save_text_file, saveImageFiles,makedirs
+                save_text_file, saveImageFiles,download_file2
 from colorama import Fore, Back, Style
-#Download
-import os
-import re
-import requests
-from tqdm import tqdm
-import time
-#
 
 # Set the URL for the API endpoint
 civitai = civitaimodels("https://civitai.com/api/v1/models?limit=10")
-
-def download_file2(folder, filename,  url):
-
-    makedirs(folder)
-    file_name = os.path.join(folder, filename)
-    #thread = threading.Thread(target=download_file, args=(url, filepath))
-
-    # Maximum number of retries
-    max_retries = 5
-
-    # Delay between retries (in seconds)
-    retry_delay = 10
-
-    exitGenerator=False
-    while not exitGenerator:
-        # Check if the file has already been partially downloaded
-        if os.path.exists(file_name):
-            # Get the size of the downloaded file
-            downloaded_size = os.path.getsize(file_name)
-
-            # Set the range of the request to start from the current size of the downloaded file
-            headers = {"Range": f"bytes={downloaded_size}-"}
-        else:
-            downloaded_size = 0
-            headers = {}
-
-        # Split filename from included path
-        tokens = re.split(re.escape('\\'), file_name)
-        file_name_display = tokens[-1]
-
-        # Initialize the progress bar
-        progressConsole = tqdm(total=1000000000, unit="B", unit_scale=True, desc=f"Downloading {file_name_display}", initial=downloaded_size, leave=False)
-
-        # Open a local file to save the download
-        with open(file_name, "ab") as f:
-            while not exitGenerator:
-                try:
-                    # Send a GET request to the URL and save the response to the local file
-                    response = requests.get(url, headers=headers, stream=True)
-
-                    # Get the total size of the file
-                    total_size = int(response.headers.get("Content-Length", 0))
-
-                    # Update the total size of the progress bar if the `Content-Length` header is present
-                    if total_size == 0:
-                        total_size = downloaded_size
-                    progressConsole.total = total_size
-                    prg = 0
-                    # Write the response to the local file and update the progress bar
-                    for chunk in response.iter_content(chunk_size=10*1024*1024):
-                        if chunk:  # filter out keep-alive new chunks
-                            f.write(chunk)
-                            progressConsole.update(len(chunk))
-                            prg += len(chunk)
-                            yield f'{round(prg/1024/1024)}MB / {round(total_size/1024/1024)}MB'
-                    downloaded_size = os.path.getsize(file_name)
-                    # Break out of the loop if the download is successful
-                    break
-                except GeneratorExit:
-                    exitGenerator=True
-                    return
-                except ConnectionError as e:
-                    # Decrement the number of retries
-                    max_retries -= 1
-
-                    # If there are no more retries, raise the exception
-                    if max_retries == 0:
-                        raise e
-
-                    # Wait for the specified delay before retrying
-                    time.sleep(retry_delay)
-        # Close the progress bar
-        exitGenerator=True
-        progressConsole.close()
-        downloaded_size = os.path.getsize(file_name)
-        # Check if the download was successful
-        if downloaded_size >= total_size:
-            print(Fore.LIGHTCYAN_EX + f"Save: {file_name_display}" + Style.RESET_ALL)
-            yield 'Downloaded'
-
-        else:
-            print(f"Error: File download failed. Retrying... {file_name_display}")
-            yield 'Failed'
-        return
-
-def updateVersionsByModelID(model_ID=None):
-    if model_ID is not None:
-        civitai.selectModelByID(model_ID)
-        if civitai.getSelectedModelIndex() is not None:
-            dict = civitai.getModelVersionsList()
-            civitai.selectVersionByName(next(iter(dict.keys()), None))
-        return gr.Dropdown.update(choices=[k for k, v in dict.items()], value=f'{next(iter(dict.keys()), None)}')
-    else:
-        return gr.Dropdown.update(choices=[],value = None)
 
 def on_ui_tabs():
     with gr.Blocks() as civitai_interface:
@@ -504,7 +402,16 @@ def on_ui_tabs():
                 grTxtPages,
                 #grTxtSaveFolder
             ])
-
+        
+        def updateVersionsByModelID(model_ID=None):
+            if model_ID is not None:
+                civitai.selectModelByID(model_ID)
+                if civitai.getSelectedModelIndex() is not None:
+                    dict = civitai.getModelVersionsList()
+                    civitai.selectVersionByName(next(iter(dict.keys()), None))
+                return gr.Dropdown.update(choices=[k for k, v in dict.items()], value=f'{next(iter(dict.keys()), None)}')
+            else:
+                return gr.Dropdown.update(choices=[],value = None)
         def eventTextUpdated(grTxtJsEvent):
             if grTxtJsEvent is not None:
                 grTxtJsEvent = grTxtJsEvent.split(':')

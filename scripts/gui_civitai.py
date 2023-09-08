8a8beb575b26a66b0d18827f722475975cf1aedc
SignalFlagZ <Signal0Flag0Z@gmail.com>
2023-09-08 23:04:34 +0900

    Revert "Separate import file_manage"
    This reverts commit 5db643692fa48f13cf024ea56ab7915944659a89.

--- scripts/gui_civitai.py
+++ scripts/gui_civitai.py
@@ -2,18 +2,16 @@ import gradio as gr
 from modules import script_callbacks
 import modules.scripts as scripts
 from scripts.civitai_api import civitaimodels
-
+from scripts.file_manage import extranetwork_folder, isExistFile,\
+                save_text_file, saveImageFiles,download_file2
 from colorama import Fore, Back, Style
 
 # Set the URL for the API endpoint
 
 class uiCivitai():
     def __init__(self, jsID:str='Index1'):
-        from scripts.file_manage import extranetwork_folder, isExistFile,\
-                save_text_file, saveImageFiles,download_file2
-        # Set the URL for the API endpoint
-        self.civitai = civitaimodels("https://civitai.com/api/v1/models?limit=16")
-        with gr.Column() as self.components:
+        self.civitai = civitaimodels("https://civitai.com/api/v1/models?limit=16",jsID=jsID)
+        with gr.Column() as self.interface:
             with gr.Row():
                 with gr.Column(scale=4):
                     grRadioContentType = gr.Radio(label='Content type:', choices=["Checkpoint","TextualInversion","LORA","LoCon","Poses","Controlnet","Hypernetwork","AestheticGradient", "VAE"], value="Checkpoint", type="value")

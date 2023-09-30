922f99ec11a5ac5260b6a2a050e9f196e17e9621
SignalFlagZ <Signal0Flag0Z@gmail.com>
2023-09-30 13:19:18 +0900

    Force card click event

--- javascript/civitai-html.js
+++ javascript/civitai-html.js
@@ -10,10 +10,18 @@ function select_model(model_name) {
 		let selector = '#civsfz_eventtext' + match[2] + ' textarea';
 		let model_dropdown = gradioApp().querySelector(selector);
 		if (model_dropdown && model_name) {
-			model_dropdown.value = model_name;
+			/*Force card click event*/
+			model_dropdown.value = model_name + ':' + getRandomIntInclusive(0, 9999);
 			updateInput(model_dropdown);
 		}
 	}
+}
+
+/*https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%E5%8C%85%E6%8B%AC%E7%9A%84%E3%81%AB_2_%E3%81%A4%E3%81%AE%E5%80%A4%E3%81%AE%E9%96%93%E3%81%AE%E3%83%A9%E3%83%B3%E3%83%80%E3%83%A0%E3%81%AA%E6%95%B4%E6%95%B0%E3%82%92%E5%BE%97%E3%82%8B*/
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
 function trigger_event(element, event) {

193fe81d47a42704dcafcf346a37775d9968f82b
SignalFlagZ <Signal0Flag0Z@gmail.com>
2024-01-30 14:36:44 +0900

    Update version

--- scripts/civsfz_ui.py
+++ scripts/civsfz_ui.py
@@ -598,7 +598,7 @@ class components():
         return self.components
 
 def on_ui_tabs():
-    ver = 'v1.9.2'
+    ver = 'v1.9.3'
     tabNames = []
     for i in range(1, opts.civsfz_number_of_tabs + 1):
         tabNames.append(f'Browser{i}')
